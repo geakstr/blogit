04c3a5bf2ca813fd219a64d95261619ad5a30aa1
Dmitry Kharitonov <geakstr@me.com>
2015-03-02 00:09:35 +0300

    Some changes and fixes

--- blogit.sh
+++ blogit.sh
@@ -3,6 +3,8 @@
 # shopt -s expand_aliases
 # alias sed=gsed
 
+markdown_lib="./markdown.pl"
+
 strindex() { 
   x="${1%%$2*}"
   [[ $x = $1 ]] && echo -1 || echo ${#x}
@@ -17,35 +19,35 @@ posts_dir="$html_dir/posts"
 
 templates_dir="./templates"
 
-post_html_template=$(< "$templates_dir/post.html")
+html_template=$(< "$templates_dir/post.html")
 
-content_pattern='{{content}}'
-content_pattern_len=${#content_pattern}
-content_pattern_pos=$(strindex "$post_html_template" "$content_pattern")
-title_pattern='{{title}}'
-title_pattern_len=${#title_pattern}
-title_pattern_pos=$(strindex "$post_html_template" "$title_pattern")
+content_pttrn='{{content}}'
+content_pttrn_len=${#content_pttrn}
+content_pttrn_pos=$(strindex "$html_template" "$content_pttrn")
+title_pttrn='{{title}}'
+title_pttrn_len=${#title_pttrn}
+title_pttrn_pos=$(strindex "$html_template" "$title_pttrn")
 
 for md_file in $md_dir/*.md
 do
   html_file=$html_dir/${md_file:$md_dir_len+1:${#md_file}-$md_dir_len-4}.html  
 
-  md_content=$(./markdown.pl $md_file)
+  md_content=$(< $md_file)
 
   title=""
-  while IFS="\n" read -ra ADDR; do
+  while IFS=$'\n' read -ra ADDR; do
     title=$(echo "${ADDR[@]}" | sed 's/^[ #]*//g' | sed 's/[ #]*$//g')
     break
   done <<< "$md_content"
   title=$(echo "$title" | sed -e 's/[\/&]/\\&/g')  
 
-  html_content=${post_html_template:0:title_pattern_pos}
+  html_content=${html_template:0:title_pttrn_pos}
   html_content=$html_content$title
-  html_content=$html_content${post_html_template:title_pattern_pos+title_pattern_len}
+  html_content=$html_content${html_template:title_pttrn_pos+title_pttrn_len}
 
-  html_content=${html_content:0:$(strindex "$html_content" "$content_pattern")}
-  html_content=$html_content$(echo "$md_content" | ./markdown.sh)
-  html_content=$html_content${post_html_template:$content_pattern_pos+content_pattern_len}
+  html_content=${html_content:0:$(strindex "$html_content" "$content_pttrn")}
+  html_content=$html_content$(echo "$md_content" | $markdown_lib)
+  html_content=$html_content${html_template:$content_pttrn_pos+content_pttrn_len}
 
   echo "$html_content" > $html_file
 done
